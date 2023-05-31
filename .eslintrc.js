defb9d9510533e040afe56acf11f22b552cb0bc0
Tamara Cook <10754072+tamaracha@users.noreply.github.com>
2023-05-31 20:32:38 +0200

    Create package

---
+++ .eslintrc.js
@@ -0,0 +1,12 @@
+module.exports = {
+  env: {
+    commonjs: true,
+    es2021: true
+  },
+  extends: 'standard',
+  overrides: [],
+  parserOptions: {
+    ecmaVersion: 'latest'
+  },
+  rules: {}
+}
