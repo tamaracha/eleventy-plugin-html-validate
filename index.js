9a6bd97981a1b9bba0ed1d9f3f3c87908ebf7ec9
Tamara Cook <10754072+tamaracha@users.noreply.github.com>
2023-06-05 22:41:07 +0200

    Remove config option, fully concentrate on config files

--- index.js
+++ index.js
@@ -4,8 +4,8 @@ const pm = require('picomatch')
 const CONFIG_PATTERN = '.htmlvalidate.(js|cjs|json)'
 const CONFIG_PATTERN_R = pm(CONFIG_PATTERN)
 
-module.exports = function (eleventyConfig, { config, format } = { format: 'text' }) {
-  const htmlValidate = new HtmlValidate(new FileSystemConfigLoader(config))
+module.exports = function (eleventyConfig, { format } = { format: 'text' }) {
+  const htmlValidate = new HtmlValidate(new FileSystemConfigLoader())
   const formatter = formatterFactory(format)
   eleventyConfig.addWatchTarget(CONFIG_PATTERN)
   eleventyConfig.on('eleventy.beforeWatch', async (changedFiles) => {
@@ -13,11 +13,12 @@ module.exports = function (eleventyConfig, { config, format } = { format: 'text'
       htmlValidate.flushConfigCache()
     }
   })
+
   eleventyConfig.on('eleventy.after', async function ({ results }) {
     const reports = await Promise.all(
       results
         .filter((r) => r.outputPath.endsWith('.html'))
-        .map((r) => htmlValidate.validateString(r.content, r.outputPath))
+        .map((r) => htmlValidate.validateString(r.content, r.inputPath))
     )
     const report = Reporter.merge(reports)
     if (!report.valid) {
